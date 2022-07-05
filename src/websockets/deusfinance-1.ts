0a7123f3243d5ed2d51d9172f048fd3abc22c06c
coinlordd <coinlord.developer@gmail.com>
2022-07-05 13:02:07 +0200

    Fix quote routing

--- src/websockets/deusfinance-1.ts
+++ src/websockets/deusfinance-1.ts
@@ -6,9 +6,13 @@ import internal from 'stream'
 import { ALL_HEDGERS, HedgerID } from '../config'
 
 const hedger = ALL_HEDGERS.find((hedger) => hedger.id === HedgerID.DEUSFinance1)
+const internalQuoteRoute = buildQuoteRoute(hedger!.id)
 
-export const wsProxy = createProxyMiddleware(hedger!.quotes_route, {
+export const wsProxy = createProxyMiddleware(internalQuoteRoute, {
   target: hedger!.ws_base_url,
+  pathRewrite: {
+    [internalQuoteRoute]: '/v1/quotes',
+  },
   changeOrigin: true,
   ws: true,
 })
@@ -19,4 +23,8 @@ export default (server: Server, app: Express) => {
     'upgrade',
     wsProxy.upgrade as unknown as (req: IncomingMessage, socket: internal.Duplex, head: Buffer) => void
   )
+}
+
+function buildQuoteRoute(hedgerId: string) {
+  return `/v1/quotes/${hedgerId}`
 }
